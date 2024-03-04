7cd7cf10e63bb84f6fad7f91236eb702495b79a1
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-03-04 13:10:28 +0100

    third tasks

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ *selection_sort - sort an array by selection sort algoritm
+ *@array: array to be sorted
+ *@size: of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j, min;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ void bubble_sort(int *array, size_t size);
 listint_t *create_listint(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
+void selection_sort(int *array, size_t size);
 int main(void);
 #endif
 
